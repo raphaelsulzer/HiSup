ec43fb27e362f864e0bf0ae4f24ad86b168b25c4
raphaelsulzer <raphael.sulzer.1@gmail.com>
2025-03-03 11:22:21 +0100

    started moving pointcloud encoder to backbones module

---
+++ hisup/backbones/point_encoder_backbone.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class PointCloudEncoder(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        
+        
+    
+    
+    
+    
+        
+        
